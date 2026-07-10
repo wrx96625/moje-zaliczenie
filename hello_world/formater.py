a2c27b235f27ade4787df78815487af9f9102af6
wrx96625 <wrx96625@student.wroclaw.merito.pl>
2026-07-10 09:24:46 +0200

    Poprawki dla lintera (PEP8 i noqa)

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -23,6 +23,7 @@ def get_formatted(msg, imie, format):
 def format_to_json(msg, imie):
     return '{"imie":"' + imie + '", "msg":"' + msg + '"}'
 
+
 def plain_text(msg, imie):
     return imie + ' ' + msg
 
